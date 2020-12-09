be68b0bfb3923c49aa971bfff0ab4a8df0ee433a
michaschwab <gjplickscoconutthunderstormb>
2020-12-09 11:12:04 -0500

    Trying out a version with two separate DOMs - one custom, one for SVG. Both are cleaner and don't require foreignObject hacks.

--- responsive-vis.js
+++ responsive-vis.js
@@ -11,9 +11,15 @@ class ResponsiveVisElement extends HTMLElement {
 
     visWidth = 600;
     visHeight = 400;
+    template = document.createElement('template');
+    element = this.element;
+    parent = this.parent; // declare variable without overriding them if they are set in parent.
+    initialized = false;
+    svgWrapper = true;
 
     constructor() {
         super();
+        console.log(this, this.parent);
         this.setAllAttributes();
         this.originalChildren = [...this.children];
     }
@@ -30,34 +36,44 @@ class ResponsiveVisElement extends HTMLElement {
     }
 
     attributeChangedCallback() {
-        this.setAllAttributes();
-        this.setContents();
+        /*this.setAllAttributes();
+        this.setContents();*/
     }
 
     setContents() {
-        this.innerHTML = this.render();
+        let html = this.render();
+        if(this.svgWrapper) {
+            html = `<svg>${html}</svg>`;
+        }
+        this.template.innerHTML = html;
+
+        if(this.template.content.firstElementChild) {
+            const element = this.template.content.firstElementChild.cloneNode(true);
+            this.element = this.svgWrapper ? element.firstChild : element;
+        } else {
+            console.log('no content found', this, this.template.innerHTML)
+        }
+
         this.renderChildren();
         this.doneRendering();
     }
 
     renderChildren() {
         if(this.originalChildren.length) {
-            this.innerHTML += `<foreignObject width="${this.visWidth}" height="${this.visHeight}" id="slot"></foreignObject>`;
-            const slot = this.querySelector('#slot');
             const provide = this.provideToChildren();
 
             this.originalChildren.forEach(child => {
                 for(const attrName in provide) {
-                    child.setAttribute(attrName, provide[attrName]);
+                    //child.setAttribute(attrName, provide[attrName]);
                     child[snakeToCamel(attrName)] = provide[attrName];
                 }
-                slot.appendChild(child);
+                child.parent = this.getChildParent() || this.parent;
             });
         }
     }
 
     provideToChildren() {
-        const skipped = ['originalChildren', 'render'];
+        const skipped = ['originalChildren', 'render', 'parent', 'element', 'template', 'initialized', 'svgWrapper'];
         const props = Object.getOwnPropertyNames(this).filter(p => !skipped.includes(p));
 
         const provide = {};
@@ -71,20 +87,40 @@ class ResponsiveVisElement extends HTMLElement {
         return '';
     }
 
-    doneRendering() {}
+    doneRendering() {
+        if(!this.initialized && this.element) {
+            this.initialized = true;
+            this.parent.appendChild(this.element);
+            console.log('adding ', this.element, ' to ', this.parent, 'for ', this);
+        } else {
+            console.log('no', this.initialized, this.element, this)
+        }
+    }
+
+    getChildParent() {
+        return this.element;
+    }
 }
 
 class ResponsiveVis extends ResponsiveVisElement {
+    parent = this.parentElement;
+    svgWrapper = false;
+
     render() {
-        return `
-                <style>
-                responsive-vis svg {
-                    position: absolute;
-                    width: ${this.visWidth}px;
-                    height: ${this.visHeight}px;
-                }
-                </style>
-                <svg></svg>`;
+        return `<div class="responsive-vis">
+                    <style>
+                    .responsive-vis svg {
+                        position: absolute;
+                        width: ${this.visWidth}px;
+                        height: ${this.visHeight}px;
+                    }
+                    </style>
+                    <svg></svg>
+                </div>`;
+    }
+
+    getChildParent() {
+        return this.element.querySelector('svg');
     }
 }
 
